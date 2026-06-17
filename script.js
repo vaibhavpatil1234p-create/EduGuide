935689d6d57ab96ee73e3adbb745767097095567
Vaibhav Patil <vaibhavpatil1234p@gmail.com>
2026-06-17 20:24:56 +0530

    Fixed fetch syntax

--- script.js
+++ script.js
@@ -150,30 +150,21 @@ return;
 
 try{
 
-const response=await fetch(
-
+const response = await fetch(
 "https://eduguide-backend-cqwb.onrender.com/api/auth/login",
-
 {
+method: "POST",
 
-method:"POST",
-
-headers:{
-
-"Content-Type":"application/json"
-
+headers: {
+"Content-Type": "application/json"
 },
 
-body:JSON.stringify({
-
+body: JSON.stringify({
 email,
-
 password
-
 })
 
 }
-
 );
 
 
